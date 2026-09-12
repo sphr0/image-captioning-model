20c3efc1e6c1dcff020d2abef2d0c6af966c35ef
sphr0 <sepehr.eb02@gmail.com>
2026-09-12 20:31:08 +0330

    add eval dataset constructor in eval_subset.py

---
+++ evaluation/eval_subset.py
@@ -0,0 +1,37 @@
+import json
+from collections import defaultdict
+from pathlib import Path
+from PIL import Image
+from torch.utils.data import Dataset
+
+class EvalDataset(Dataset):
+    def __init__(self, img_dir, json_path):
+        self.img_dir = Path(img_dir)
+
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+
+        # self.image
+        self.images = data["images"]
+
+        # self.references
+        self.references = defaultdict(list)
+        for ann in data["annotations"]:
+            self.references[ann["image_id"]].append(ann["caption"])
+        
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        id = image["id"]
+        file_name = image["file_name"]
+        image_path = self.img_dir / file_name
+        image = Image.open(image_path).convert('RGB')
+
+        return {
+            "image": image,
+            "image_id": id,
+            "file_name": file_name,
+            "references": self.references[id]
+        }
